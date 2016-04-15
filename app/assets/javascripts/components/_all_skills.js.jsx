17d6f6f371d6dbdeff93cb80c23d6b386335ae57
Edgar Duran <edgar.eduran@gmail.com>
2016-04-15 12:29:33 -0600

    creact finished

---
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -0,0 +1,24 @@
+var AllSkills = React.createClass({
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+  onUpdate(skill) {
+  this.props.onUpdate(skill);
+  },
+  render() {
+    var skills = this.props.skills.map((skill, index) => {
+      return(
+      <div key={index}>
+        <Skill skill={skill}
+               handleDelete={this.handleDelete.bind(this, skill.id)}
+               handleUpdate={this.onUpdate}/>
+      </div>
+      );
+    });
+    return(
+      <div>
+        {skills}
+      </div>
+    );
+  }
+});
---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,7 @@
+var Header = React.createClass({
+  render() {
+    return(
+      <h1>Header, child of Main</h1>
+    )
+  }
+});
